d2ebb5d50206d3008a0113032f1f7bc8f5dbc43e
alchemiata <lummoire@gmail.com>
2026-07-05 01:57:13 -0400

    Remove skin selector from game screen props

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -1,4 +1,4 @@
-import type { BackgroundKey, SettingsState, TrackedCardState } from "../types";
+import type { SettingsState, TrackedCardState } from "../types";
 import CardTracker from "./CardTracker";
 import LifeCounter from "./LifeCounter";
 
@@ -9,7 +9,6 @@ interface GameScreenProps {
   onChangeLife: (delta: number) => void;
   onResetLife: () => void;
   onStartingLifeChange: (life: number) => void;
-  onBackgroundChange: (background: BackgroundKey) => void;
   onAddCard: () => void;
   onUpdateCard: (id: string, patch: Partial<Omit<TrackedCardState, "id">>) => void;
   onResetCard: (id: string) => void;
@@ -19,8 +18,20 @@ interface GameScreenProps {
 function GameScreen(props: GameScreenProps) {
   return (
     <section className="game-shell min-h-0 flex-1 md:grid md:grid-cols-[minmax(22rem,0.85fr)_1.15fr] md:gap-5">
-      <LifeCounter life={props.life} settings={props.settings} onChangeLife={props.onChangeLife} onReset={props.onResetLife} onStartingLifeChange={props.onStartingLifeChange} onBackgroundChange={props.onBackgroundChange} />
-      <CardTracker cards={props.cards} onAddCard={props.onAddCard} onUpdateCard={props.onUpdateCard} onResetCard={props.onResetCard} onDeleteCard={props.onDeleteCard} />
+      <LifeCounter
+        life={props.life}
+        settings={props.settings}
+        onChangeLife={props.onChangeLife}
+        onReset={props.onResetLife}
+        onStartingLifeChange={props.onStartingLifeChange}
+      />
+      <CardTracker
+        cards={props.cards}
+        onAddCard={props.onAddCard}
+        onUpdateCard={props.onUpdateCard}
+        onResetCard={props.onResetCard}
+        onDeleteCard={props.onDeleteCard}
+      />
     </section>
   );
 }
