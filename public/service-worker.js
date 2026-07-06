8089b8bbb5041d50d1464172df5fb9a3be79de51
alchemiata <alchemiata@users.noreply.github.com>
2026-07-05 22:18:00 -0400

    Remove stale service worker cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,66 +1,13 @@
-const CACHE_NAME = "mtg-buddy-v3";
-const ASSETS = [
-  "./",
-  "index.html",
-  "manifest.webmanifest",
-  "icons/icon-192.svg",
-  "icons/icon-512.svg",
-  "backgrounds/forest-custom.png",
-  "backgrounds/island-custom.png",
-  "backgrounds/swamp-custom.png",
-  "backgrounds/mountain-custom.png",
-  "backgrounds/plains-custom.png",
-  "backgrounds/colorless.svg",
-  "backgrounds/arcane.svg",
-  "backgrounds/dark-marble.svg",
-  "backgrounds/wood-table.svg",
-];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-  self.skipWaiting();
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches
-      .keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") {
-    return;
-  }
-
-  if (event.request.mode === "navigate" || event.request.destination === "script" || event.request.destination === "style") {
-    event.respondWith(
-      fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match(event.request).then((cached) => cached || caches.match("index.html")))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) {
-        return cached;
-      }
-
-      return fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match("index.html"));
-    })
+    Promise.all([
+      caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))),
+      self.registration.unregister(),
+      self.clients.claim(),
+    ])
   );
 });
